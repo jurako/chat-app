cf1c45376c04f97def04aa11c2535e270b9a6f40
Jurako <jurako91@gmail.com>
2025-06-01 20:53:53 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return ( 
+    <>
+      Hello World
+
+      <p className="inter-test-regular">Hi David, have you got the project report pdf?</p>
+
+      <p className="inter-test-semi-bold">Lisa Roy</p>
+    </>
+  )
+}
+
+export default App;
