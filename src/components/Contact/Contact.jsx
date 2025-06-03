719f736c74c199bd16e20877f4dff209471978a9
Jurako <jurako91@gmail.com>
2025-06-03 23:03:14 +0300

    add props

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,28 @@
 import NoUnreadIcon from './../../assets/icons/no-unread-icon.svg?react';
 import './Contact.css';
 
-function Contact() {
+function Contact({ contact }) {
   return (
     <div className="contact">
-      <img src="/public/images/avatars/contact1.png" alt="Lisa Roy" />
+      <img src={`/public/images/avatars/${contact.avatarUrl}`} alt={contact.name} />
       <div className="contact__details-container">
         <div className="contact__details">
-          <span className="contact__name">Lisa Roy</span>
-          <span className="contact__last-msg-time">10:35 AM</span>
+          <span className="contact__name">{contact.name}</span>
+          <span className="contact__last-msg-time">{contact.lastMessageTime}</span>
         </div>
         <div className="contact__details">
-          <span className="contact__last-msg">Hi, are you Available Tomorrow?</span>
-          <div className="contact__msg-status"><NoUnreadIcon/></div>
+          <span className="contact__last-msg">{contact.lastMessage}</span>
+          <div
+            className={
+              "contact__msg-status" +
+              (contact.unreadMessages ? " contact__msg-status_has-unread" : "")
+          }>
+            {
+              contact.unreadMessages ?
+              contact.unreadMessages :
+              <NoUnreadIcon/>
+            }
+          </div>
         </div>
       </div>
     </div>
