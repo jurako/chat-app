a971e4c4d400eb338d78719fe3e124e515f4f798
Jurako <jurako91@gmail.com>
2025-06-03 23:04:09 +0300

    display contacts list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react'
+
+import initialContacts from './contacts';
 import SearchIcon from './assets/icons/search-icon.svg?react';
 
+import Contact from './components/Contact/Contact'
 import Profile from './components/Profile/Profile'
 import Input from './components/shared/Input/Input'
 import './App.css'
 
 function App() {
+  const [contacts, setContacts] = useState(initialContacts);
+
+  let contactItems = null;
+  if(contacts.length) {
+    contactItems = contacts.map(contact =>
+      <Contact key={contact.id} contact={contact} />
+    );
+  }
+
   return ( 
     <div className="wrapper">
       <aside>
@@ -14,6 +27,12 @@ function App() {
           name="search"
           iconBefore={SearchIcon}
         />
+        {contactItems && (
+          <div className="contacts">
+            {contactItems}
+          </div>
+        )}
+
       </aside>
       <main>
         {/* <span>Select a conversation</span> */}
