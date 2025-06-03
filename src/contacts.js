d0a7e740bf774aba338dbeff3e538ce5451d32d7
Jurako <jurako91@gmail.com>
2025-06-03 22:59:45 +0300

    add mokup contacts

---
+++ src/contacts.js
@@ -0,0 +1,60 @@
+const contacts = [
+  {
+    id: 1,
+    name: 'Dianne Jhonson',
+    lastMessage: 'Ya. I’ll be adding more team members to it.',
+    lastMessageTime: '10:35 PM',
+    unreadMessages: null,
+    avatarUrl: 'contact1.png'
+  },
+  {
+    id: 2,
+    name: 'Lisa Roy',
+    lastMessage: 'Hi, are you Available Tomorrow?',
+    lastMessageTime: '10:35 PM',
+    unreadMessages: 1,
+    avatarUrl: 'contact2.png'
+  },
+  {
+    id: 3,
+    name: 'Jamie Taylor',
+    lastMessage: "Nice One.\nWill Do it tommorow",
+    lastMessageTime: '10:35 PM',
+    unreadMessages: 3,
+    avatarUrl: 'contact3.png'
+  },
+  {
+    id: 4,
+    name: 'Jason Roy',
+    lastMessage: "That’s Great. I am Looking forward to having a great start.",
+    lastMessageTime: '10:35 PM',
+    unreadMessages: null,
+    avatarUrl: 'contact4.png'
+  },
+  {
+    id: 5,
+    name: 'Amy Frost',
+    lastMessage: "Hi, will you start working on the chat app right now?",
+    lastMessageTime: '10:35 PM',
+    unreadMessages: null,
+    avatarUrl: 'contact5.png'
+  },
+  {
+    id: 6,
+    name: 'Paul Wilson',
+    lastMessage: "See you tommorow champ",
+    lastMessageTime: '10:35 PM',
+    unreadMessages: null,
+    avatarUrl: 'contact6.png'
+  },
+  {
+    id: 7,
+    name: 'Ana Williams',
+    lastMessage: "??",
+    lastMessageTime: '10:35 PM',
+    unreadMessages: 1,
+    avatarUrl: 'contact7.png'
+  },
+];
+
+export default contacts;
