4c0864a5b513a57969e3bc670688286dae671a17
Jurako <jurako91@gmail.com>
2025-06-05 23:07:22 +0300

    add mokup messages

---
+++ src/messages.js
@@ -0,0 +1,40 @@
+const messages = [
+  {
+    id: 1,
+    authorName: 'Dianne Johnson',
+    text: 'Hi David, have you got the project report pdf?',
+    userId: 2
+  },
+  {
+    id: 2,
+    authorName: 'David Peters',
+    text: 'NO. I did not get it',
+    userId: 1
+  },
+  {
+    id: 3,
+    authorName: 'Dianne Johnson',
+    text: 'Ok, I will just sent it here. Plz be sure to fill the details by today end of the day.',
+    userId: 2
+  },
+  {
+    id: 4,
+    authorName: 'David Peters',
+    text: 'Ok. Should I send it over email as well after filling the details.',
+    userId: 1
+  },
+  {
+    id: 5,
+    authorName: 'Dianne Johnson',
+    text: 'Ya. I’ll be adding more team members to it.',
+    userId: 2
+  },
+  {
+    id: 6,
+    authorName: 'David Peters',
+    text: 'OK',
+    userId: 1
+  },
+];
+
+export default messages;
